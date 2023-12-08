4780f15ad936c9c5e1dc27ec767b396de5d066ba
Shivamprajapati970 <shivamprajapati15981@gmail.com>
2023-12-08 11:30:33 +0530

    Python Blog App Development environment setup complete and create blog home and about page then showing post, create super user Some work is still gowing on an model

---
+++ my_site/blogs/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+posts=[
+    {
+        "author":"Shivam Prajapati",
+        "date_posted":datetime.now(),
+        "title":"First Post",
+        "content":"This is a sample post",
+    },
+    {
+        "author":"Shivam Prajapati",
+        "date_posted":datetime.now(),
+        "title":"Second Post",
+        "content":"This is a sample post",
+    }
+
+]
+
+# Create your views here.
+def home(request):
+    context={
+        'title':'HOME PAGE',
+        'posts':posts,
+    }
+    return render(request, template_name='blogs/home.html',context=context)
+
+def about(request):
+    context={
+        'title':'ABOUT PAGE'
+    }
+    return render(request, template_name='blogs/about.html',context=context)
